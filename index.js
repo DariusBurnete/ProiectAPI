3c8f621af712d20d7393341462e5dfb889e09585
E.on <90399175+DariusBurnete@users.noreply.github.com>
2024-04-18 21:33:38 +0300

    Version 1.3

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 const port = 3000;
 const apiKey = process.env.API_KEY;
 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -23,19 +24,39 @@ app.get('/list', async(req, res) =>{
         let response;
         do {
             response = await axios.get(
-                `http://www.omdbapi.com/?s=movie&page=${page}&apikey=${apiKey}`
+                `http://www.omdbapi.com/?s=man&page=${page}&apikey=${apiKey}`
             );
             movies = movies.concat(response.data.Search || []);
             page++;
         } while (response.data.Search && response.data.Search.length !== 0 && page <= 80);
-        res.render("list.ejs", {activePage: 'list', movies});
+        res.render("list.ejs", {activePage: "list", movies});
     } catch (error){
         console.error(error);
     }
 });
 
 app.get('/sort', (req, res) =>{
-    res.render("sort.ejs", {activePage: "sort"});
+    res.render("sort.ejs", { activePage: "sort" });
+});
+
+app.post('/sort', async(req, res) =>{
+    let movies = [];
+    const year = req.body.year;
+    const type = req.body.type;
+    try{
+        let page = 1;
+        let response;
+        do{
+            response = await axios.get(
+                `http://www.omdbapi.com/?s=man&y=${year}&page=${page}&type=${type}&apikey=${apiKey}`
+            );
+            movies = movies.concat(response.data.Search || []);
+            page++;
+        } while(response.data.Search && response.data.Search.length !== 0 && page <= 3);
+        res.render("sort.ejs", { activePage: "sort", movies });
+    } catch (error){
+        console.error(error);
+    }
 });
 
 app.get('/search', (req, res) =>{
